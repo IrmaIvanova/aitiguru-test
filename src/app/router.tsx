6f14f28d99690520fb941a353a12afcb644da093
IrmaIvanova <ii@pryaniky.ru>
2026-03-16 15:16:29 +0300

    refactor: implement feature-based architecture

---
+++ src/app/router.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import LoginPage from "../pages/LoginPage/LoginPage";
+// import ProductsPage from "../pages/ProductsPage/ProductsPage";
+
+export const Router = () => (
+  <BrowserRouter>
+    <Routes>
+      {/* <Route path="/login" element={<LoginPage />} />
+      <Route path="/products" element={<ProductsPage />} /> */}
+    </Routes>
+  </BrowserRouter>
+);
